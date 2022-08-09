8a9ee3ab468ad147a6518fcf3c4eafa9f5ec4633
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2022-08-09 10:51:44 -0500

    Logo part 2

--- src/common/Logo/Logo.jsx
+++ src/common/Logo/Logo.jsx
@@ -4,12 +4,12 @@ import styled from 'styled-components';
 import LogoSVG from "../../assets/logo.svg";
 
 const LogoStyle = styled.img`
-  width: 50px;
-  height: 50px;
+
+  max-height: 50px;
 `;
 
-const Logo = () => (
-  <LogoStyle src={LogoSVG} alt="logo" />
+const Logo = ({ size }) => (
+  <LogoStyle src={LogoSVG} alt="logo" height={size} />
 );
 
 export default Logo;
--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -3,10 +3,7 @@ import React from 'react';
 import Logo from '../../common/Logo/Logo';
 
 const Header = () => (
-  <>
-    <Logo />
-    <h1>LSC</h1>
-  </>
+  <Logo />
 );
 
 export default Header;
