824f55e330be3468d90fae55f6b82917aaf8ed1d
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2022-08-09 17:07:44 -0500

    Header part 4

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Logo from '../../common/Logo/Logo';
 import Navigation from '../Navigation/Navigation';
 
+const HeaderStyle = styled.header`
+  display: flex;
+  align-items: center ;
+  padding: 40px 0;
+  @media (max-width: 640px) {
+    padding: 20px;
+  }
+`;
+
 const Header = () => (
   <div className="container mx-auto">
-    <div div className="flex align-middle py-8" >
+    <HeaderStyle>
       <Logo size="30px" />
       <Navigation />
-    </div>
+    </HeaderStyle>
   </div>
 );
 
