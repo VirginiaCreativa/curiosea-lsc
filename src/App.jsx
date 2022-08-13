506c5f4031a396b2e218b51c79d258f7806acfad
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2022-08-12 20:40:52 -0500

    Background

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Layout from './layouts/Layout';
 
+const Background = styled.div`
+  background: linear-gradient(0deg, rgba(0,65,130, 0.2) 0%, rgba(255,255,255,0) 100%);
+  height: 100vh;
+`;
 function App() {
   return (
-    <Layout />
+    <Background>
+      <Layout />
+    </Background>
   );
 }
 
