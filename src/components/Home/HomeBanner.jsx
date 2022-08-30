950319d799d4ea7ed194079206aa43be3422a2a8
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2022-08-30 11:31:49 -0500

    Add Page Home and Implement Routes

---
+++ src/components/Home/HomeBanner.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HomeBanner = () => (
+  <div className="container mx-auto">
+    <h1>Banner</h1>
+  </div>
+);
+
+export default HomeBanner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
+import Banner from '../components/Home/HomeBanner';
+
 function Home() {
   return (
-    <div>
-
-    </div>
+    <Banner />
   );
 }
 
