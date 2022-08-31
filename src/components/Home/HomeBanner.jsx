c421bf61104436fc84312930bd06baa2572ef690
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2022-08-31 10:52:23 -0500

    Implement Repsonsive part 3

--- src/components/Home/HomeBanner.jsx
+++ src/components/Home/HomeBanner.jsx
@@ -6,12 +6,14 @@ import ImgWoman from '../../assets/woman.png';
 const Banner = styled.div`
   display: flex;
   justify-content: space-between;
-  height: 400px;
+  min-height: 600px;
 `;
 
 const BoxImg = styled.div`
-
+  display: flex;
+  justify-content: center ;
 `;
+
 const Img = styled.img`
   max-height: 100%;
 `;
@@ -21,14 +23,14 @@ const InfoBanner = styled.div`
 `;
 
 const HomeBanner = () => (
-  <div className="mx-auto">
-    <Banner className="flex">
-      <InfoBanner className="basis-2/4">
+  <div className="md:mx-auto">
+    <Banner className="flex-col sm:flex-row">
+      <InfoBanner className="basis-full sm:basis-2/4">
         <h1>Quieres aprender LSC</h1>
         <h1>Aquí te enseñamos</h1>
         <p>Ipsum eu incididunt ullamco irure enim duis laboris magna nulla labore. Amet ullamco labore tempor anim minim eu. </p>
       </InfoBanner>
-      <BoxImg className="basis-2/4 flex justify-end">
+      <BoxImg className="basis-full sm:basis-2/4">
         <Img src={ImgWoman} alt="" />
       </BoxImg>
     </Banner>
