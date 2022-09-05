fe4ae334f16e031e56baa60ba392f5118159e1bb
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2022-09-05 16:25:44 -0500

    Selections Home part 6

--- src/common/Sections/SectionHeadingTitle.jsx
+++ src/common/Sections/SectionHeadingTitle.jsx
@@ -3,16 +3,23 @@ import styled from 'styled-components';
 
 import Variable from '../../styles/variable';
 
+const Headign = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+
 const Title = styled.h1`
   color: ${Variable.blue};
   text-transform: uppercase;
 `;
 
 const SectionHeadingTitle = ({ title, subtitle }) => (
-  <div className="mx-auto my-24 flex flex-col justify-center">
+  <Headign className="mx-auto">
     <Title>{title}</Title>
     <h4>Ut eiusmod est irure labore ex consequat nostrud minim ullamco eu.</h4>
-  </div>
+  </Headign>
 );
 
 export default SectionHeadingTitle;
